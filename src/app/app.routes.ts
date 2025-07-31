6a442e09a5c1d11a2234cc410b48156977204947
rambu <rambuwanitsireledzo@gmail.com>
2025-07-31 21:06:06 +0200

    Add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AddressBookComponent } from './features/address-books/components/address-book/address-book.component';
+import { AddressBooksComponent } from './features/address-books/pages/address-books/address-books.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path:'', pathMatch: 'full', redirectTo:"addressBooks" },
+    { path:'addressBooks', component: AddressBooksComponent },
+    { path :'addressBook/id', component:AddressBookComponent },
+    {path : '***' , redirectTo:"addressBooks"}
+];
